1ae5642376ee435dbd39aeed037b81ceb272f74a
Andres Vega <handresvegarodriguez@gmail.com>
2022-09-23 14:30:42 -0400

    This Course of CI only works with the PHP version 5.6 because the flashdata session only works with this version of PHP

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -4,14 +4,14 @@
 class Users extends CI_Controller
 {
 
-    public function show($user_id)
-    {
+    // public function show($user_id)
+    // {
 
-        // $this->load->model('user_model');
-        $data['results']  = $this->user_model->get_users($user_id, 'suave');
+    //     // $this->load->model('user_model');
+    //     $data['results']  = $this->user_model->get_users($user_id, 'suave');
 
 
-        $this->load->view('user_view', $data);
+    //     $this->load->view('user_view', $data);
 
 
 
@@ -19,43 +19,95 @@ class Users extends CI_Controller
         // foreach ($result as $object) {
         //     echo $object->username . "<br>";
         // }
-    }
+    // }
 
-    public function insert()
-    {
+    // public function insert()
+    // {
 
-        $username = "peater";
-        $password = "secret";
+    //     $username = "peater";
+    //     $password = "secret";
 
-        $this->user_model->create_users([
+    //     $this->user_model->create_users([
 
+    //         'username' => $username,
+    //         'password' => $password
+
+    //     ]);
+    // }
+
+    // public function update()
+    // {
+
+    //     $id = 3;
+
+    //     $username = "Willian";
+    //     $password = "not so secret";
+
+    //     $this->user_model->update_users([
+
+    //         'username' => $username,
+    //         'password' => $password
+
+    //     ], $id);
+    // }
+
+    // public function delete()
+    // {
+
+    //     $id = 3;
+
+    //     $this->user_model->delete_users($id);
+    // }
+
+
+
+    public function login()
+{
+
+    $this->form_validation->set_rules('username','Username','trim|required|min_length[3]');
+
+    $this->form_validation->set_rules('password','Password','trim|required|min_length[3]');
+
+    $this->form_validation->set_rules('confirm_password','Confirm Password','trim|required|min_length[3]|matches[password]');
+
+    if ($this->form_validation->run() == FALSE){
+
+        $data = array('main_view' => 'home_view');
+        $this->load->view('layouts/main', $data);
+
+    }else{
+
+       $username = $this->input->post('username');
+       $password = $this->input->post('password');
+
+       $user_id = $this->user_model->login_user($username, $password);
+
+       if($user_id){
+
+        $user_data = array(
+
+            'user_id' => $user_id,
             'username' => $username,
-            'password' => $password
+            'logged_in' => true
 
-        ]);
+        );
+
+        $this->session->set_userdata($user_data);
+        
+        $this->session->set_flashdata('login_success', 'You are now logged in');
+
+        redirect('home/index');
+
+
+       }else{
+        
+        $this->session->set_flashdata('login_failed', 'Sorry you are not logged in');
+
+        redirect('home/index');
+
+
+       }
+
     }
-
-    public function update()
-    {
-
-        $id = 3;
-
-        $username = "Willian";
-        $password = "not so secret";
-
-        $this->user_model->update_users([
-
-            'username' => $username,
-            'password' => $password
-
-        ], $id);
-    }
-
-    public function delete()
-    {
-
-        $id = 3;
-
-        $this->user_model->delete_users($id);
-    }
+}
 }
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,1 +1,88 @@
 <h2>Login form</h2>
+
+<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal');?>
+
+<?php echo validation_errors(); ?> 
+
+<?php echo form_open('users/login', $attributes); ?>
+
+<div class="form-group">
+
+    <?php echo form_label('Username');?>
+
+    <?php 
+    
+        $data = array (
+            
+            'class' => 'form-control',
+            'name' => 'username',
+            'placeholder' => 'Enter Username'
+
+        );
+    
+    ?>
+
+    <?php echo  form_input($data)?>
+</div>
+
+
+<div class="form-group">
+
+    <?php echo form_label('Password');?>
+
+    <?php 
+    
+        $data = array (
+            
+            'class' => 'form-control',
+            'name' => 'password',
+            'placeholder' => 'Enter Password'
+
+        );
+    
+    ?>
+
+    <?php echo  form_password($data)?>
+</div>
+
+
+<div class="form-group">
+
+    <?php echo form_label('Confirm Password');?>
+
+    <?php 
+    
+        $data = array (
+            
+            'class' => 'form-control',
+            'name' => 'confirm_password',
+            'placeholder' => 'Confirm Password'
+
+        );
+    
+    ?>
+
+    <?php echo  form_password($data)?>
+</div>
+
+
+<div class="form-group">
+
+    <?php 
+    
+        $data = array (
+            
+            'class' => 'btn btn-primary',
+            'name' => 'submit',
+            'value' => 'Login'
+
+        );
+    
+    ?>
+
+    <?php echo  form_submit($data)?>
+</div>
+
+
+
+<?php echo form_close(); ?>
